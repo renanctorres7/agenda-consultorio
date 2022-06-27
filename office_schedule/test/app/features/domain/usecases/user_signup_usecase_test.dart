0f679bf51ee6599dd21385a778cba5556d476a9d
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-26 21:26:23 -0300

    Update user_signup_usecase_test.dart

--- office_schedule/test/app/features/domain/usecases/user_signup_usecase_test.dart
+++ office_schedule/test/app/features/domain/usecases/user_signup_usecase_test.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:office_schedule/app/features/domain/entities/entities.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
 import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
 import 'package:office_schedule/app/features/domain/usecases/user_signup_usecase.dart';
 
@@ -22,12 +23,12 @@ void main() {
     verify(() => repository.signUp(params)).called(1);
   });
 
-  test("Should return an Failure Error", () async {
+  test("Should return Failure Error", () async {
     when(() => repository.signUp(params))
-        .thenAnswer((_) async => Right(params));
+        .thenAnswer((_) async => Left(ServerError()));
     final result = await usecase(params: params);
 
-    expect(result, Right(params));
+    expect(result, Left(ServerError()));
     verify(() => repository.signUp(params)).called(1);
   });
 }
