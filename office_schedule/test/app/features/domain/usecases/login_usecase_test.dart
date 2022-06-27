fc9128a75468b135a2db38a3f18f94c19c46e2f5
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-26 23:36:53 -0300

    Create initial login usecase test

--- office_schedule/test/app/features/domain/usecases/login_usecase_test.dart
+++ office_schedule/test/app/features/domain/usecases/login_usecase_test.dart
@@ -1,7 +1,21 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/entities/entities.dart';
+import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
+import 'package:office_schedule/app/features/domain/usecases/usecases.dart';
+
+class LoginRepositoryMock extends Mock implements LoginRepository {}
+
+class LoginEntityMock extends Mock implements LoginEntity {}
 
 void main() {
-  testWidgets('login usecase ...', (tester) async {
-    // TODO: Implement test
+  final repository = LoginRepositoryMock();
+  final usecase = LoginUsecaseImpl(repository);
+  final loginEntity = LoginEntityMock();
+
+  test("Should return Login Entity", () async {
+    when(() => repository.login(loginEntity))
+        .thenAnswer((invocation) async => Right(entity));
   });
 }
