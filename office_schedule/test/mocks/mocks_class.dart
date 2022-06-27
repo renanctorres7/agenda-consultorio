9e8a323c5009b5511258483554a17e9ff24618ee
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-26 23:38:24 -0300

    Create mocks_class.dart

---
+++ office_schedule/test/mocks/mocks_class.dart
@@ -0,0 +1,6 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/entities/entities.dart';
+
+class LoginEntityMock extends Mock implements LoginEntity {}
+
+class SignUpEntityMock extends Mock implements SignUpEntity {}
