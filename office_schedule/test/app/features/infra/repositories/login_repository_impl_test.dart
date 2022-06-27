941c94bfa25763c7aad3065ac225009088bec21a
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-27 00:07:59 -0300

    Create "Should return a Datasource Error if gets error" test

--- office_schedule/test/app/features/infra/repositories/login_repository_impl_test.dart
+++ office_schedule/test/app/features/infra/repositories/login_repository_impl_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
 import 'package:office_schedule/app/features/infra/datasources/datasources.dart';
 import 'package:office_schedule/app/features/infra/repositories/repositories.dart';
 
@@ -21,6 +22,26 @@ void main() {
     final result = await repository.login(loginEntity);
 
     expect(result, Right(signUpModel));
+    verify(() => datasource.login(loginEntity)).called(1);
+  });
+
+  test("Should return a Null Error if gets null", () async {
+    when(() => datasource.login(loginEntity))
+        .thenAnswer((invocation) async => null);
+
+    final result = await repository.login(loginEntity);
+
+    expect(result, Left(NullError()));
+    verify(() => datasource.login(loginEntity)).called(1);
+  });
+
+  test("Should return a Datasource Error if gets error", () async {
+    when(() => datasource.login(loginEntity))
+        .thenThrow(Left(DataSourceError()));
+
+    final result = await repository.login(loginEntity);
+
+    expect(result, Left(DataSourceError()));
     verify(() => datasource.login(loginEntity)).called(1);
   });
 }
