7f58ec7be8690280c769ec6ffbb8af9fef7a0950
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-26 23:44:16 -0300

    Create "Should return Domain Error when gets error" test and create Domain Error

--- office_schedule/lib/app/features/domain/errors/failure_error.dart
+++ office_schedule/lib/app/features/domain/errors/failure_error.dart
@@ -2,6 +2,11 @@ import 'package:equatable/equatable.dart';
 
 abstract class FailureError extends Equatable {}
 
+class DomainError extends FailureError {
+  @override
+  List<Object?> get props => [];
+}
+
 class ServerError extends FailureError {
   @override
   List<Object?> get props => [];
--- office_schedule/test/app/features/domain/usecases/login_usecase_test.dart
+++ office_schedule/test/app/features/domain/usecases/login_usecase_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
 import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
 import 'package:office_schedule/app/features/domain/usecases/usecases.dart';
 
@@ -21,6 +22,17 @@ void main() {
     final result = await usecase(loginEntity);
 
     expect(result, Right(signUpEntity));
+
+    verify(() => repository.login(loginEntity)).called(1);
+  });
+
+  test("Should return Domain Error when gets error", () async {
+    when(() => repository.login(loginEntity))
+        .thenAnswer((invocation) async => Left(DomainError()));
+
+    final result = await usecase(loginEntity);
+
+    expect(result, Left(DomainError()));
 
     verify(() => repository.login(loginEntity)).called(1);
   });
