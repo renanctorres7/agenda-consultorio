21137b6f9c2eb2df060e0f7fcee599f0c5d80cc2
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-29 15:37:02 -0300

    Update users_list_repository_impl.dart

--- office_schedule/lib/app/features/infra/repositories/users_list_repository_impl.dart
+++ office_schedule/lib/app/features/infra/repositories/users_list_repository_impl.dart
@@ -3,13 +3,21 @@ import 'package:office_schedule/app/features/domain/errors/failure_error.dart';
 import 'package:office_schedule/app/features/domain/entities/users_entity.dart';
 
 import 'package:dartz/dartz.dart';
+import 'package:office_schedule/app/features/infra/datasources/datasources.dart';
 
 import '../../domain/repositories/respositories.dart';
 
 class UsersListRepositoryImpl extends UsersListRepository {
+  final UsersListDatasource datasource;
+
+  UsersListRepositoryImpl(this.datasource);
   @override
-  Future<Either<FailureError, List<UsersEntity>>> getUsersList() {
-    // TODO: implement getUsersList
-    throw UnimplementedError();
+  Future<Either<FailureError, List<UsersEntity>>> getUsersList() async {
+    try {
+      final result = await datasource.getUsersList();
+      return result != null ? Right(result) : Left(NullError());
+    } catch (e) {
+      return Left(DataSourceError());
+    }
   }
 }
