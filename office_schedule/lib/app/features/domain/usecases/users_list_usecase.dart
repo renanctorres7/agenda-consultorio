ce86cf44a225f28c5bfd96a59eba225ed7485702
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-29 14:07:41 -0300

    Update users_list_usecase.dart

--- office_schedule/lib/app/features/domain/usecases/users_list_usecase.dart
+++ office_schedule/lib/app/features/domain/usecases/users_list_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
 
 import '../entities/entities.dart';
 import '../errors/errors.dart';
@@ -8,8 +9,12 @@ abstract class UsersListUsecase {
 }
 
 class UsersListUsecaseImpl extends UsersListUsecase {
+  final UsersListRepository repository;
+
+  UsersListUsecaseImpl(this.repository);
   @override
-  Future<Either<FailureError, UsersEntity>> call() {
-    return;
+  Future<Either<FailureError, List<UsersEntity>>> call() async {
+    final result = await repository.getUsersList();
+    return result;
   }
 }
