fab4c8f61cb0ad7668c00b5ece940e61c1676aa5
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-06-29 13:32:34 -0300

    Update get_user_repository_impl.dart

--- office_schedule/lib/app/features/infra/repositories/get_user_repository_impl.dart
+++ office_schedule/lib/app/features/infra/repositories/get_user_repository_impl.dart
@@ -2,11 +2,20 @@ import 'package:office_schedule/app/features/domain/errors/failure_error.dart';
 import 'package:office_schedule/app/features/domain/entities/users_entity.dart';
 import 'package:dartz/dartz.dart';
 import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
+import 'package:office_schedule/app/features/infra/datasources/get_user_datasource.dart';
 
 class GetUserRepositoryImpl extends GetUserRepository {
+  final GetUserDatasource datasource;
+
+  GetUserRepositoryImpl(this.datasource);
   @override
-  Future<Either<FailureError, UsersEntity>> getUserProfile(String objectId) {
-    // TODO: implement getUserProfile
-    throw UnimplementedError();
+  Future<Either<FailureError, UsersEntity>> getUserProfile(
+      String objectId) async {
+    try {
+      final result = await datasource.getUserProfile(objectId);
+      return result != null ? Right(result) : Left(NullError());
+    } catch (e) {
+      return Left(DataSourceError());
+    }
   }
 }
