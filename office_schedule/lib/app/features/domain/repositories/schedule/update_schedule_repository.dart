d6922c7716473da1efc20a7cdef1fac1c408849f
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 10:48:59 -0300

    Create update_schedule_repository.dart

---
+++ office_schedule/lib/app/features/domain/repositories/schedule/update_schedule_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+
+import '../../entities/entities.dart';
+import '../../errors/errors.dart';
+import '../../success/success.dart';
+
+abstract class UpdateScheduleRepository {
+  Future<Either<FailureError, Success>> updateSchedule(
+      ScheduleUpdateEntity scheduleUpdateEntity);
+}
