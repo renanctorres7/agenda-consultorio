19a44a67cec06999de31842e3e3005d47254d46d
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 13:53:45 -0300

    Create schedule_period_model.dart

---
+++ office_schedule/lib/app/features/infra/models/schedules/period/schedule_period_model.dart
@@ -0,0 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:office_schedule/app/features/domain/entities/entities.dart';
+
+part 'schedule_period_model.g.dart';
+
+@JsonSerializable()
+class SchedulePeriodModel extends SchedulePeriodEntity {
+  SchedulePeriodModel({required super.page, required super.period});
+
+  factory SchedulePeriodModel.fromJson(Map<String, dynamic> json) =>
+      _$SchedulePeriodModelFromJson(json);
+  Map<String, dynamic> toJson() => _$SchedulePeriodModelToJson(this);
+}
--- office_schedule/lib/app/features/infra/models/schedules/schedules.dart
+++ office_schedule/lib/app/features/infra/models/schedules/schedules.dart
@@ -1,3 +1,4 @@
 export './create/schedule_create_model.dart';
 export './update/schedule_update_model.dart';
 export './schedule_model.dart';
+export './period/schedule_period_model.dart';
