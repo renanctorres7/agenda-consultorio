57a96909898e19f8dc0eedaacd9fdd768a811a97
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 15:42:55 -0300

    Create search_schedule_date_repository.dart

--- office_schedule/lib/app/features/domain/repositories/schedule/schedule.dart
+++ office_schedule/lib/app/features/domain/repositories/schedule/schedule.dart
@@ -2,3 +2,4 @@ export './create_schedule_repository.dart';
 export './update_schedule_repository.dart';
 export './list_schedule_period_repository.dart';
 export './list_schedule_filter_repository.dart';
+export './search_schedule_date_repository.dart';
