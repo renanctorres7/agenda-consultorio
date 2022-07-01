d65722e4e4c7c35c7a26f01f46d64cfe8daa1912
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 14:20:40 -0300

    Create List Schedule Period tests

--- office_schedule/test/app/features/infra/repositories/schedules/list_schedule_period_repository_impl_test.dart
+++ office_schedule/test/app/features/infra/repositories/schedules/list_schedule_period_repository_impl_test.dart
@@ -1,7 +1,51 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
+import 'package:office_schedule/app/features/infra/datasources/schedules/list_schedule_period_datasource.dart';
+import 'package:office_schedule/app/features/infra/models/models.dart';
+import 'package:office_schedule/app/features/infra/repositories/schedules/schedules.dart';
+
+class ListSchedulePeriodDatasourceMock extends Mock
+    implements ListSchedulePeriodDatasource {}
 
 void main() {
-  testWidgets('list schedule period repository impl ...', (tester) async {
-    // TODO: Implement test
+  final datasource = ListSchedulePeriodDatasourceMock();
+  final repository = ListSchedulePeriodRepositoryImpl(datasource);
+  List<ScheduleModel> list = [];
+  SchedulePeriodModel periodModel = SchedulePeriodModel(page: 0, period: 7);
+
+  test('Should input a Schedule Period Model and return a Schedule Model list',
+      () async {
+    when(() => datasource.getScheduleListByPeriod(periodModel))
+        .thenAnswer((_) async => list);
+
+    final result = await repository.getScheduleListByPeriod(periodModel);
+
+    expect(result, Right(list));
+
+    verify(() => datasource.getScheduleListByPeriod(periodModel)).called(1);
+  });
+
+  test('Should returns Datasource Error if gets error', () async {
+    when(() => datasource.getScheduleListByPeriod(periodModel))
+        .thenThrow(DataSourceError());
+
+    final result = await repository.getScheduleListByPeriod(periodModel);
+
+    expect(result, Left(DataSourceError()));
+
+    verify(() => datasource.getScheduleListByPeriod(periodModel)).called(1);
+  });
+
+  test('Should returns Null Error if gets null value', () async {
+    when(() => datasource.getScheduleListByPeriod(periodModel))
+        .thenAnswer((_) async => null);
+
+    final result = await repository.getScheduleListByPeriod(periodModel);
+
+    expect(result, Left(NullError()));
+
+    verify(() => datasource.getScheduleListByPeriod(periodModel)).called(1);
   });
 }
