286b6216f8fe4c54fc76f6685bc3164b90cd1933
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 14:38:37 -0300

    Create ListScheduleFilterUsecaseImpl

--- office_schedule/lib/app/features/domain/repositories/schedule/schedule.dart
+++ office_schedule/lib/app/features/domain/repositories/schedule/schedule.dart
@@ -1,3 +1,4 @@
 export './create_schedule_repository.dart';
 export './update_schedule_repository.dart';
 export './list_schedule_period_repository.dart';
+export './list_schedule_filter_repository.dart';
--- office_schedule/lib/app/features/domain/usecases/schedule/list_schedule_filter_usecase.dart
+++ office_schedule/lib/app/features/domain/usecases/schedule/list_schedule_filter_usecase.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 
 import '../../entities/entities.dart';
 import '../../errors/errors.dart';
+import '../../repositories/respositories.dart';
 
 abstract class ListScheduleFilterUsecase {
   Future<Either<FailureError, List<ScheduleEntity>>> call(
@@ -9,10 +10,15 @@ abstract class ListScheduleFilterUsecase {
 }
 
 class ListScheduleFilterUsecaseImpl implements ListScheduleFilterUsecase {
+  final ListScheduleFilterRepository repository;
+
+  ListScheduleFilterUsecaseImpl(this.repository);
   @override
   Future<Either<FailureError, List<ScheduleEntity>>> call(
-      ScheduleFilterEntity scheduleFilterEntity) {
-    // TODO: implement call
-    throw UnimplementedError();
+      ScheduleFilterEntity scheduleFilterEntity) async {
+    final result =
+        await repository.getListScheduleByFilter(scheduleFilterEntity);
+
+    return result;
   }
 }
