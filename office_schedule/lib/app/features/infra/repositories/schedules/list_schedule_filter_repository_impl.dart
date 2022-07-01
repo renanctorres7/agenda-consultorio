20fd156c8d40cc73fc69a4e4707abdb163f78a97
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 15:13:18 -0300

    Update list_schedule_filter_repository_impl.dart

--- office_schedule/lib/app/features/infra/repositories/schedules/list_schedule_filter_repository_impl.dart
+++ office_schedule/lib/app/features/infra/repositories/schedules/list_schedule_filter_repository_impl.dart
@@ -3,12 +3,22 @@ import 'package:office_schedule/app/features/domain/entities/schedules/schedule_
 import 'package:office_schedule/app/features/domain/entities/schedules/schedule_entity.dart';
 import 'package:dartz/dartz.dart';
 import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
+import 'package:office_schedule/app/features/infra/datasources/schedules/list_schedule_filter_datasource.dart';
 
 class ListScheduleFilterRepositoryImpl extends ListScheduleFilterRepository {
+  final ListScheduleFilterDatasource datasource;
+
+  ListScheduleFilterRepositoryImpl(this.datasource);
   @override
   Future<Either<FailureError, List<ScheduleEntity>>> getListScheduleByFilter(
-      ScheduleFilterEntity scheduleFilterEntity) {
-    // TODO: implement getListScheduleByFilter
-    throw UnimplementedError();
+      ScheduleFilterEntity scheduleFilterEntity) async {
+    try {
+      final result =
+          await datasource.getListScheduleByFilter(scheduleFilterEntity);
+
+      return result != null ? Right(result) : Left(NullError());
+    } catch (e) {
+      return Left(DataSourceError());
+    }
   }
 }
