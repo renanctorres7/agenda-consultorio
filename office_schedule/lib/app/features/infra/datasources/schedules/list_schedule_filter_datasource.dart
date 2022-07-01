3c8e51725ed62380505abfa532d5e75a9f7b2761
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 15:18:16 -0300

    Update list_schedule_filter_datasource.dart

--- office_schedule/lib/app/features/infra/datasources/schedules/list_schedule_filter_datasource.dart
+++ office_schedule/lib/app/features/infra/datasources/schedules/list_schedule_filter_datasource.dart
@@ -1,6 +1,8 @@
+import 'package:office_schedule/app/features/infra/models/models.dart';
+
 import '../../../domain/entities/entities.dart';
 
 abstract class ListScheduleFilterDatasource {
-  Future<List<ScheduleEntity>?> getListScheduleByFilter(
+  Future<List<ScheduleModel>?> getListScheduleByFilter(
       ScheduleFilterEntity scheduleFilterEntity);
 }
