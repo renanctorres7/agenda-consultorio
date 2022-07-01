8ea598a811877299718c96394f431b314d0dcb65
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-01 13:17:54 -0300

    Create List Schedule Period Usecase Implementation

--- office_schedule/lib/app/features/domain/usecases/schedule/list_schedule_period_usecase.dart
+++ office_schedule/lib/app/features/domain/usecases/schedule/list_schedule_period_usecase.dart
@@ -7,3 +7,12 @@ abstract class ListSchedulePeriodUsecase {
   Future<Either<FailureError, List<ScheduleEntity>>> call(
       SchedulePeriodEntity schedulePeriodEntity);
 }
+
+class ListSchedulePeriodUsecaseImpl extends ListSchedulePeriodUsecase {
+  @override
+  Future<Either<FailureError, List<ScheduleEntity>>> call(
+      SchedulePeriodEntity schedulePeriodEntity) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
