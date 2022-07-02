4dc91b73c946394c5705d29de937b5d0ff6588b6
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-02 15:42:27 -0300

    Update search_schedule_date_repository_impl.dart

--- office_schedule/lib/app/features/infra/repositories/schedules/search_schedule_date_repository_impl.dart
+++ office_schedule/lib/app/features/infra/repositories/schedules/search_schedule_date_repository_impl.dart
@@ -5,14 +5,23 @@ import 'package:office_schedule/app/features/domain/entities/schedules/schedule_
 import 'package:office_schedule/app/features/domain/entities/schedules/schedule_date_entity.dart';
 
 import 'package:dartz/dartz.dart';
+import 'package:office_schedule/app/features/infra/datasources/schedules/schedules.dart';
 
 import '../../../domain/repositories/respositories.dart';
 
 class SearchScheduleDateRepositoryImpl implements SearchScheduleDateRepository {
+  final SearchScheduleDateDatasource datasource;
+
+  SearchScheduleDateRepositoryImpl(this.datasource);
   @override
   Future<Either<FailureError, List<ScheduleEntity>>> searchScheduleFromDate(
-      ScheduleDateEntity scheduleDateEntity) {
-    // TODO: implement searchScheduleFromDate
-    throw UnimplementedError();
+      ScheduleDateEntity scheduleDateEntity) async {
+    try {
+      final result =
+          await datasource.searchScheduleFromDate(scheduleDateEntity);
+      return result != null ? Right(result) : Left(NullError());
+    } catch (e) {
+      return Left(DataSourceError());
+    }
   }
 }
