b4db65995eabdad6a676772c1e301c3135f19198
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-04 17:00:49 -0300

    Create dio_update_profile_datasource.dart

---
+++ office_schedule/lib/app/features/external/external_dio/datasource/users/dio_update_profile_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:office_schedule/app/features/domain/entities/login/sign_up_entity.dart';
+import 'package:office_schedule/app/features/external/external_dio/external_dio.dart';
+import 'package:office_schedule/app/features/infra/datasources/datasources.dart';
+import 'package:office_schedule/app/features/infra/models/sign_up/sign_up_model.dart';
+
+class DioUpdateProfileDatasource implements UpdateProfileDatasource {
+  final UpdateProfileService updateProfileService;
+  final String sessionToken;
+
+  DioUpdateProfileDatasource(this.updateProfileService, this.sessionToken);
+  @override
+  Future<SignUpModel?> updateProfile(SignUpEntity entity) async {
+    try {
+      final result = await updateProfileService.updateProfile(
+          sessionToken, SignUpModel.fromEntity(entity));
+      return result;
+    } on DioError catch (e) {
+      throw DioError(requestOptions: e.requestOptions, response: e.response);
+    }
+  }
+}
