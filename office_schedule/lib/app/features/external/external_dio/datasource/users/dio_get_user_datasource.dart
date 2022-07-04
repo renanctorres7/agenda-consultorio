498936ffdcda6d3497eb037862b9fcfa55c3dd11
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-04 16:42:02 -0300

    create dio_get_user_datasource.dart

---
+++ office_schedule/lib/app/features/external/external_dio/datasource/users/dio_get_user_datasource.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:office_schedule/app/features/external/external_dio/external_dio.dart';
+import 'package:office_schedule/app/features/infra/datasources/datasources.dart';
+import 'package:office_schedule/app/features/infra/models/users/users_model.dart';
+
+class DioGetUserDatasource implements GetUserDatasource {
+  final GetUserService getUserService;
+
+  DioGetUserDatasource(this.getUserService);
+  @override
+  Future<UsersModel?> getUserProfile(String objectId) async {
+    Map<String, String> mapObjectId = {'objectId': objectId};
+    try {
+      final result = await getUserService.getUserProfile(mapObjectId);
+      return result;
+    } on DioError catch (e) {
+      throw DioError(requestOptions: e.requestOptions, response: e.response);
+    }
+  }
+}
--- office_schedule/lib/app/features/external/external_dio/service/service.dart
+++ office_schedule/lib/app/features/external/external_dio/service/service.dart
@@ -1,1 +1,2 @@
 export './login/login.dart';
+export './users/users.dart';
