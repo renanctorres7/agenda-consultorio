3e4ddcfdc3e63abc9362f23fc0fd6a1c7b85b0c0
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-07 10:38:32 -0300

    Create create_schedule_service.g.dart

---
+++ office_schedule/lib/app/features/external/external_dio/service/schedule/create_schedule_service.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_schedule_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+class _CreateScheduleService implements CreateScheduleService {
+  _CreateScheduleService(this._dio, {this.baseUrl});
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<void> createSchedule(sessionToken, scheduleCreateModel) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{
+      r'X-Parse-Application-Id': '',
+      r'X-Parse-REST-API-Key': '',
+      r'Content-Type': 'application/json',
+      r'X-Parse-Session-Token': sessionToken
+    };
+    _headers.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    _data.addAll(scheduleCreateModel.toJson());
+    await _dio.fetch<void>(_setStreamType<void>(Options(
+            method: 'POST',
+            headers: _headers,
+            extra: _extra,
+            contentType: 'application/json')
+        .compose(_dio.options, '/create-schedule',
+            queryParameters: queryParameters, data: _data)
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    return null;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
