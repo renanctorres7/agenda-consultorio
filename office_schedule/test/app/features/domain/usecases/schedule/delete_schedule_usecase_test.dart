538f1efd70045c9ef95f5c853f366d1cd85a0db1
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-06 22:28:00 -0300

    Create delete_schedule_usecase_test.dart

---
+++ office_schedule/test/app/features/domain/usecases/schedule/delete_schedule_usecase_test.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
+import 'package:office_schedule/app/features/domain/repositories/respositories.dart';
+import 'package:office_schedule/app/features/domain/success/success.dart';
+import 'package:office_schedule/app/features/domain/usecases/schedule/delete_schedule_usecase.dart';
+
+class DeleteScheduleRepositoryMock extends Mock
+    implements DeleteScheduleRepository {}
+
+void main() {
+  final repository = DeleteScheduleRepositoryMock();
+  final usecase = DeleteScheduleUsecaseImpl(repository);
+  String objectId = faker.guid.guid();
+  test('Should input a object id and return a Successful Response ', () async {
+    when(() => repository.deleteSchedule(objectId))
+        .thenAnswer((_) async => Right(SuccessfulResponse()));
+
+    final result = await usecase(objectId);
+
+    expect(result, Right(SuccessfulResponse()));
+
+    verify(() => repository.deleteSchedule(objectId)).called(1);
+  });
+
+  test('Should return a Domain Error if gets error ', () async {
+    when(() => repository.deleteSchedule(objectId))
+        .thenAnswer((_) async => Left(DomainError()));
+
+    final result = await usecase(objectId);
+
+    expect(result, Left(DomainError()));
+
+    verify(() => repository.deleteSchedule(objectId)).called(1);
+  });
+}
