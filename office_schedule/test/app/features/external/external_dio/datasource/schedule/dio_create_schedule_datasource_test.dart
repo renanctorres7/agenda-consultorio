9219f040f64974d6ee5a08097d2f7c5d8838469f
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-07 13:13:41 -0300

    Create dio_create_schedule_datasource_test.dart

---
+++ office_schedule/test/app/features/external/external_dio/datasource/schedule/dio_create_schedule_datasource_test.dart
@@ -0,0 +1,57 @@
+import 'package:dio/dio.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/success/success.dart';
+import 'package:office_schedule/app/features/external/external_dio/datasource/schedule/dio_create_schedule_datasource.dart';
+import 'package:office_schedule/app/features/external/external_dio/service/schedule/create_schedule_service.dart';
+import 'package:office_schedule/app/features/infra/models/models.dart';
+
+import '../../../../../../mocks/mocks.dart';
+
+class CreateScheduleServiceMock extends Mock implements CreateScheduleService {}
+
+class ScheduleCreateModelMock extends Mock implements ScheduleCreateModel {}
+
+void main() {
+  String sessionToken = faker.guid.guid();
+  late CreateScheduleServiceMock service;
+  late DioCreateScheduleDatasource datasource;
+
+  final scheduleCreateModel = MocksModels.scheduleCreateModel;
+  final modelMock = ScheduleCreateModelMock();
+
+  setUp(() async {
+    registerFallbackValue(modelMock);
+
+    registerFallbackValue(scheduleCreateModel);
+    registerFallbackValue(sessionToken);
+    service = CreateScheduleServiceMock();
+    datasource = DioCreateScheduleDatasource(
+        createScheduleService: service, sessionToken: sessionToken);
+  });
+
+  test('When input a Create Schedule Model should return a Successful Response',
+      () async {
+    when(() => service.createSchedule(any(), any())).thenAnswer((_) async {});
+
+    final result = await datasource.createSchedule(scheduleCreateModel);
+
+    expect(result, SuccessfulResponse());
+  });
+
+  test('Should return DataSource Error if gets error', () async {
+    when(() => service.createSchedule(any(), any())).thenThrow(DioError(
+      requestOptions: RequestOptions(path: ''),
+      response: Response(
+        data: 'Something went wrong',
+        statusCode: 404,
+        requestOptions: RequestOptions(path: ''),
+      ),
+    ));
+
+    final result = datasource.createSchedule(scheduleCreateModel);
+
+    expect(result, throwsA(const TypeMatcher<DioError>()));
+  });
+}
