26b56b1cbf6e8fc1dc275ad7df122d92358c4418
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-08 11:15:38 -0300

    Create search_schedule_date_service.g.dart

---
+++ office_schedule/lib/app/features/external/external_dio/service/schedule/search_schedule_date_service.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_schedule_date_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+class _SearchScheduleDateService implements SearchScheduleDateService {
+  _SearchScheduleDateService(this._dio, {this.baseUrl});
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<List<ScheduleModel>?> searchScheduleFromDate(
+      sessionToken, scheduleDateModel) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{
+      r'X-Parse-Application-Id': '',
+      r'X-Parse-REST-API-Key': '',
+      r'Content-Type': 'application/json',
+      r'X-Parse-Session-Token': sessionToken
+    };
+    _headers.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    _data.addAll(scheduleDateModel.toJson());
+    final _result = await _dio.fetch<List<dynamic>>(
+        _setStreamType<List<ScheduleModel>>(Options(
+                method: 'POST',
+                headers: _headers,
+                extra: _extra,
+                contentType: 'application/json')
+            .compose(_dio.options, '/search-schedules-date',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    var value = _result.data
+        ?.map((dynamic i) => ScheduleModel.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
