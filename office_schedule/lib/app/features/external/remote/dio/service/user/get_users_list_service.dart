10d41445172bcb749140cc13ec16093b3cf2df5e
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-13 19:08:25 -0300

    Create Users List: Dio Datasource and Services

---
+++ office_schedule/lib/app/features/external/remote/dio/service/user/get_users_list_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/http.dart';
+
+import '../../../../../../core/core.dart';
+import '../../../../../../core/endpoints/parse_endpoints.dart';
+import '../../../../../infra/models/models.dart';
+
+@RestApi(baseUrl: Environments.baseUrl)
+abstract class GetUsersListService {
+  factory GetUsersListService(Dio dio) => GetUsersListService(dio);
+
+  @POST(ParseEndpoints.getUsersList)
+  @Headers(ParseHeaders.header)
+  Future<List<UserModel>?> getUsersList(
+      @Header(ParseHeaders.sessionTokenTitle) String sessionToken);
+}
