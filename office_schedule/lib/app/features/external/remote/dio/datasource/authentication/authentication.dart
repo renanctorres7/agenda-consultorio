3064fd3018d64164223f052587143ad02d27de5b
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-13 15:34:48 -0300

    Create: Dio Validate Token Datasource and unit tests

--- office_schedule/lib/app/features/external/remote/dio/datasource/authentication/authentication.dart
+++ office_schedule/lib/app/features/external/remote/dio/datasource/authentication/authentication.dart
@@ -2,3 +2,4 @@ export './dio_login_datasource.dart';
 export './dio_change_password_datasource.dart';
 export './dio_reset_password_datasource.dart';
 export './dio_sign_up_datasource.dart';
+export './dio_validate_token_datasource.dart';
