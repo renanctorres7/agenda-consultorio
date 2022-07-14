a18dfb16d066724ec5b1181c013318511d47ab4a
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-14 17:53:50 -0300

    Create save_storage_token_repository_impl_test.dart

---
+++ office_schedule/test/app/features/infra/repositories/storage/token/save_storage_token_repository_impl_test.dart
@@ -0,0 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
+import 'package:office_schedule/app/features/domain/success/success.dart';
+import 'package:office_schedule/app/features/infra/datasources/storage/storage.dart';
+import 'package:office_schedule/app/features/infra/repositories/storage/storage.dart';
+
+class SaveStorageTokenDatasourceMock extends Mock
+    implements SaveStorageTokenDatasource {}
+
+void main() {
+  final datasource = SaveStorageTokenDatasourceMock();
+  final repository = SaveStorageTokenRepositoryImpl(datasource);
+
+  final key = faker.guid.guid();
+  final value = faker.guid.guid();
+
+  test("Should input a Key and a Value and return a Successful Response",
+      () async {
+    when(() => datasource.saveStorageToken(key: key, value: value))
+        .thenAnswer((_) async => SuccessfulResponse());
+
+    final result = await repository.saveStorageToken(key: key, value: value);
+
+    expect(result, Right(SuccessfulResponse()));
+    verify(() => datasource.saveStorageToken(key: key, value: value)).called(1);
+  });
+
+  test("Should return a Datasource Error if gets error", () async {
+    when(() => datasource.saveStorageToken(key: key, value: value))
+        .thenThrow(Left(DataSourceError()));
+
+    final result = await repository.saveStorageToken(key: key, value: value);
+
+    expect(result, Left(DataSourceError()));
+    verify(() => datasource.saveStorageToken(key: key, value: value)).called(1);
+  });
+}
