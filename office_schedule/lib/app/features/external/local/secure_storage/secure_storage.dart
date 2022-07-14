b005eed33381141bf8e7bc08d319003fd3719e8f
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-14 12:06:31 -0300

    Update secure_storage.dart

--- office_schedule/lib/app/features/external/local/secure_storage/secure_storage.dart
+++ office_schedule/lib/app/features/external/local/secure_storage/secure_storage.dart
@@ -1,19 +1,19 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-abstract class SecureStorage {
-  Future<void> writeStorageValue(
+class SecureStorage {
+  static Future<void> writeStorageValue(
       {required String key, required String value}) async {
     const storage = FlutterSecureStorage();
     await storage.write(key: key, value: value);
   }
 
-  Future<String> readStorageValue({required String key}) async {
+  static Future<String> readStorageValue({required String key}) async {
     const storage = FlutterSecureStorage();
     String? value = await storage.read(key: key);
     return value ?? '';
   }
 
-  Future<void> deleteStorageValue({required String key}) async {
+  static Future<void> deleteStorageValue({required String key}) async {
     const storage = FlutterSecureStorage();
     await storage.delete(key: key);
   }
