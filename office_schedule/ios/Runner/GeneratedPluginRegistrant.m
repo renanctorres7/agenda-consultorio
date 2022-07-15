bb3ba19c1630732c40622e9eb9e71d14c553d672
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-15 11:34:50 -0300

    flutter updates

---
+++ office_schedule/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,34 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+<<<<<<< Updated upstream
+#if __has_include(<flutter_secure_storage/FlutterSecureStoragePlugin.h>)
+#import <flutter_secure_storage/FlutterSecureStoragePlugin.h>
+#else
+@import flutter_secure_storage;
+#endif
+
+=======
+>>>>>>> Stashed changes
+#if __has_include(<path_provider_ios/FLTPathProviderPlugin.h>)
+#import <path_provider_ios/FLTPathProviderPlugin.h>
+#else
+@import path_provider_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+<<<<<<< Updated upstream
+  [FlutterSecureStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSecureStoragePlugin"]];
+=======
+>>>>>>> Stashed changes
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
