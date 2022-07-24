62275373900397e4dc7b61398722071d3f92c065
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-24 16:47:24 -0300

    Create Get Schedule usecase tests

---
+++ office_schedule/test/app/features/domain/usecases/schedule/get_schedule_usecase_test.dart
@@ -0,0 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:office_schedule/app/features/domain/errors/errors.dart';
+import 'package:office_schedule/app/features/domain/repositories/schedule/get_schedule_repository.dart';
+import 'package:office_schedule/app/features/domain/usecases/schedule/schedule.dart';
+
+import '../../../../../mocks/mocks.dart';
+
+class GetScheduleRepositoryMock extends Mock implements GetScheduleRepository {}
+
+void main() {
+  final repository = GetScheduleRepositoryMock();
+  final usecase = GetScheduleUsecaseImpl(repository);
+  final responseBody = MocksModels.scheduleModel;
+
+  String objectId = faker.guid.guid();
+  test("Should input an object ID and return a Schedule Entity", () async {
+    when(() => repository.getSchedule(objectId: objectId))
+        .thenAnswer((_) async => Right(responseBody));
+
+    final result = await usecase(objectId: objectId);
+
+    expect(result, Right(responseBody));
+
+    verify(() => repository.getSchedule(objectId: objectId)).called(1);
+  });
+
+  test("Should return Domain Error when gets error", () async {
+    when(() => repository.getSchedule(objectId: objectId))
+        .thenAnswer((_) async => Left(DomainError()));
+
+    final result = await usecase(objectId: objectId);
+
+    expect(result, Left(DomainError()));
+
+    verify(() => repository.getSchedule(objectId: objectId)).called(1);
+  });
+}
