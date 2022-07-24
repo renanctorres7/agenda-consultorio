4318a23038ef0dffa2719594bc55ab7059751c12
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-24 16:10:04 -0300

    Create Get Schedule usecase and repository

--- office_schedule/lib/app/features/domain/usecases/schedule/schedule.dart
+++ office_schedule/lib/app/features/domain/usecases/schedule/schedule.dart
@@ -4,3 +4,4 @@ export './list_schedule_period_usecase.dart';
 export './list_schedule_filter_usecase.dart';
 export './search_schedule_date_usecase.dart';
 export './delete_schedule_usecase.dart';
+export './get_schedule_usecase.dart';
