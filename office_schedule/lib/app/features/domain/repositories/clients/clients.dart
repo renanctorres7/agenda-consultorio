06e60a885cdf0bad39fe24296c885734a6e29bd5
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-25 09:38:58 -0300

    Create Clients Update usecase and repository

--- office_schedule/lib/app/features/domain/repositories/clients/clients.dart
+++ office_schedule/lib/app/features/domain/repositories/clients/clients.dart
@@ -1,1 +1,2 @@
 export './clients_create_repository.dart';
+export './clients_update_repository.dart';
--- office_schedule/lib/app/features/domain/usecases/clients/clients.dart
+++ office_schedule/lib/app/features/domain/usecases/clients/clients.dart
@@ -1,1 +1,2 @@
 export './clients_create_usecase.dart';
+export './clients_update_usecase.dart';
