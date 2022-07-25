b260f811a324f4822bc8ec9c09d4a70f8c9b1fcd
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-25 09:51:46 -0300

    Create Clients Update repository implementation and datasource

--- office_schedule/lib/app/features/infra/datasources/clients/clients.dart
+++ office_schedule/lib/app/features/infra/datasources/clients/clients.dart
@@ -1,1 +1,2 @@
 export './clients_create_datasource.dart';
+export './clients_update_datasource.dart';
--- office_schedule/lib/app/features/infra/repositories/clients/clients.dart
+++ office_schedule/lib/app/features/infra/repositories/clients/clients.dart
@@ -1,1 +1,2 @@
 export './clients_create_repository_impl.dart';
+export './clients_update_repository_impl.dart';
