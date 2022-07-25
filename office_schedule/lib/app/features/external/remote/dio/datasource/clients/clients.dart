43b4a2df8b70130ae303e5ab25dba31667790c07
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-25 15:12:18 -0300

    Create Get Clients List datasource implementation and unit test

--- office_schedule/lib/app/features/external/remote/dio/datasource/clients/clients.dart
+++ office_schedule/lib/app/features/external/remote/dio/datasource/clients/clients.dart
@@ -1,3 +1,4 @@
 export './dio_create_client_datasource.dart';
 export './dio_update_client_datasource.dart';
 export './dio_get_client_datasource.dart';
+export './dio_get_clients_list_datasource.dart';
