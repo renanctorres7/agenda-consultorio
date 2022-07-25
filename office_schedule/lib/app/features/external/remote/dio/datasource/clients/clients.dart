02449b26dd7f20e5c5d7eaeae0b0edbfc243ded5
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-07-25 16:00:05 -0300

    Create a Delete Client dio datasource and unit tests

--- office_schedule/lib/app/features/external/remote/dio/datasource/clients/clients.dart
+++ office_schedule/lib/app/features/external/remote/dio/datasource/clients/clients.dart
@@ -2,3 +2,4 @@ export './dio_create_client_datasource.dart';
 export './dio_update_client_datasource.dart';
 export './dio_get_client_datasource.dart';
 export './dio_get_clients_list_datasource.dart';
+export './dio_delete_client_datasource.dart';
