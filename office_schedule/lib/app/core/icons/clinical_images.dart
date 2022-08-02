35436aa9acade6f92cb50ca3830ee7c00bcf2f21
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-08-02 17:25:11 -0300

    Create clinical_images.dart

---
+++ office_schedule/lib/app/core/icons/clinical_images.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const String _path = 'assets/images';
+
+class ClinicalImages {
+  static const String psychologistIllustration =
+      '$_path/ psychologistIllustration.png';
+
+  static loadImages(BuildContext context) async {
+    Future.wait([
+      precacheImage(const AssetImage(psychologistIllustration), context),
+    ]);
+  }
+}
